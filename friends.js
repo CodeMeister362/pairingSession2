549705f812ebff1bef5699693bae7d49ab9d0c01
Nathan Chamberlain <chamberlainuxdesign@gmail.com>
2023-01-11 15:37:03 -0800

    Adds another array

--- friends.js
+++ friends.js
@@ -2,3 +2,6 @@ var arrays = ["nate", "scott", "josh", "kyle", "tim"];
 console.log(arrays.slice(3, -1));
 console.log(arrays.slice(-1));
 console.log(arrays.slice(0,1));
+
+var placesToVisit = ["canada", "costa rica", "mexico", "tajikiston", "maskova"]
+console.log(placesToVisit.slice(2, 5));
